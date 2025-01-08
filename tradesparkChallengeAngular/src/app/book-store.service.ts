758a5bf67f173da5300d0c149a21fdc305b069f3
leopoldo <villaleoo@hotmail.com>
2025-01-08 11:03:06 -0300

    [FRONTEND] agregado types.ts y algunas lineas al servicio de libros

--- tradesparkChallengeAngular/src/app/book-store.service.ts
+++ tradesparkChallengeAngular/src/app/book-store.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Book } from './types';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookStoreService {
+    private BASE_URL= 'http://localhost:8000/bookStore/';
+    private primaryData: Book[] = [];
+    private dataSubject: BehaviorSubject<Book[]> = new BehaviorSubject<Book[]>([]);
 
-  constructor(private client: HttpClient) { }
+    constructor(private client: HttpClient) { }
 
-  getBooks() {
-    return this.client.get('http://localhost:8000/bookStore/books/')
-  }
+    getBooks() {
+      return this.client.get(`${this.BASE_URL}books/`)
+    }
 }
---
+++ tradesparkChallengeAngular/src/app/types.ts
@@ -0,0 +1,21 @@
+export interface Category {
+    id: number;
+    name: string;
+    description: string | null;
+}
+
+export interface Author {
+    id: number;
+    name: string;
+    bio: string | null;
+    date_of_birth: Date | null;
+}
+
+export interface Book {
+    id: number;
+    title: string;
+    author: Author;
+    categories: Category[] | null;
+    publication_date: Date | null;
+    ISBN: string | null;
+}
