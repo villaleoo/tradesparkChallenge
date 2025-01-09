65f413129a2763de461c11e802406f411756b4da
leopoldo <villaleoo@hotmail.com>
2025-01-09 17:12:34 -0300

    [BACKEND] agregado algunos tests unitarios al endpoint /books/:id/categories/ en el archivo test.py

--- tradesparkChallangeDjango/simpleBookStore/mixins.py
+++ tradesparkChallangeDjango/simpleBookStore/mixins.py
@@ -2,6 +2,6 @@ class CapitalizeFieldsMixin:
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         for field, value in representation.items():
-            if isinstance(value, str) and value:  # Verifica que sea string y no esté vacío
+            if isinstance(value, str) and value:  
                 representation[field] = value.capitalize()
         return representation
--- tradesparkChallangeDjango/simpleBookStore/tests.py
+++ tradesparkChallangeDjango/simpleBookStore/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Book, Category, Author
 
-# Create your tests here.
+
+class BookCategoriesTest(APITestCase):
+
+    def mock_data(self):
+        self.category = Category.objects.create(name="Testing")
+        self.author = Author.objects.create(name="Anonymous")
+        self.book = Book.objects.create(title="essential tests", author=self.author)
+        self.book.categories.add(self.category)
+
+        self.url = reverse('categories', kwargs={'pk': self.book.pk}) 
+    
+    def test_add_category_to_book(self):
+        new_category = Category.objects.create(name='The test category')
+        
+        response = self.client.patch(self.url, {'category_name': 'The test category'}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertIn(new_category, self.book.categories.all())  
+
+    def test_remove_category_to_book(self):
+        response = self.client.patch(self.url,{'category_name': "Testing"},format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book.refresh_from_db()
+        self.assertNotIn(self.category,self.book.categories.all())
+
+    def test_book_not_found(self):
+        url_not_found=reverse('categories',kwargs={'pk':2873})
+
+        response= self.client.patch(url_not_found,{'category_name': 'The test category'},format='json')
+
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['message'], 'Book not found.')
+
+    def test_category_not_found(self):
+        response = self.client.patch(self.url, {'category_name': 'Not found category'}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data['message'], "No category found for 'not found category'.")
+    
+   
